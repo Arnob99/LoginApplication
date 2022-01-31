e04d3b765f97bfcb8f458590332706170ccf3c67
Arnob99 <t.k.arnob.999@gmail.com>
2022-01-31 14:40:28 +0600

    added an error message

--- app/src/main/java/com/example/loginapplication/LoginPage.java
+++ app/src/main/java/com/example/loginapplication/LoginPage.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginPage extends AppCompatActivity {
 
@@ -18,6 +19,7 @@ public class LoginPage extends AppCompatActivity {
         Button login_btn = findViewById(R.id.login_btn);
         EditText user_textview = findViewById(R.id.user_textView_login);
         EditText pass_textview = findViewById(R.id.pass_textView);
+        TextView error_textview = findViewById(R.id.error_textview);
 
         login_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,9 +28,13 @@ public class LoginPage extends AppCompatActivity {
                 String password = pass_textview.getText().toString();
 
                 if (username.length() > 0 && password.length() > 0) {
+                    error_textview.setVisibility(View.INVISIBLE);
                     Intent home_page_intent = new Intent(getApplicationContext(), HomePage.class);
                     home_page_intent.putExtra("com.example.loginapplication.Arnob", username);
                     startActivity(home_page_intent);
+                }
+                else {
+                    error_textview.setVisibility(View.VISIBLE);
                 }
             }
         });
